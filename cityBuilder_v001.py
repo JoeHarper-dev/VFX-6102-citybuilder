257da7e5f2a9376a29373fad84da35f4d22006a3
ᴋᴜɪ <kui@eeu.jp>
2025-03-10 15:48:21 +0000

    Update cityBuilder_v001.py

--- cityBuilder_v001.py
+++ cityBuilder_v001.py
@@ -1,9 +1,21 @@
+from re import X
+#-----------------------------------------------------------------------------------------------#
+#         ,-----.,--.  ,--.               ,-----.          ,--.,--.   ,--.                      #
+#        '  .--./`--',-'  '-.,--. ,--.    |  |) /_ ,--.,--.`--'|  | ,-|  | ,---. ,--.--.        #
+#        |  |    ,--.'-.  .-' \  '  /     |  .-.  \|  ||  |,--.|  |' .-. || .-. :|  .--'        #
+#        '  '--'\|  |  |  |    \   '      |  '--' /'  ''  '|  ||  |\ `-' |\   --.|  |           #
+#         `-----'`--'  `--'  .-'  /       `------'  `----' `--'`--' `---'  `----'`--'           #
+#                            `---'                                                              #
+#-----------------------------------------------------------------------------------------------#
+
+
 from PySide2 import QtCore
 from PySide2 import QtWidgets
 import hou
 import json
 import os
 import random
+
 
 class cityBuilder(QtWidgets.QWidget):
     def __init__(self, parent=None):
@@ -99,8 +111,9 @@ class cityBuilder(QtWidgets.QWidget):
 
 #------------------------------------------------------------------------------------------------------------------
 # Snap to ground
-
+        '''
 # Label
+        
         hboxSnap = QtWidgets.QHBoxLayout()
         self.LabelSnap = QtWidgets.QLabel(
             'snap to ground?'
@@ -108,30 +121,156 @@ class cityBuilder(QtWidgets.QWidget):
         self.LabelSnap.setMinimumWidth(25)
         hboxSnap.addWidget(self.LabelSnap)
 
+#check box
         self.checkBoxSnap = QtWidgets.QCheckBox()
         self.checkBoxSnap.setMinimumWidth(25)
         hboxSnap.addWidget(self.checkBoxSnap)
-        self.vBox.addLayout(hboxSnap)
+        self.vBox.addLayout(hboxSnap)     
+        '''   
+
+#label
+        hboxSnapMenu = QtWidgets.QHBoxLayout()
+        self.ddlLabel = QtWidgets.QLabel(
+            'object to snap to'
+        )
+        self.ddlLabel.setMinimumWidth(175)
+        hboxSnapMenu.addWidget(self.ddlLabel)
         
+#drop down
+        self.DdlSnap = QtWidgets.QComboBox()
+        self.DdlSnap.addItem('None')
+        self.DdlSnap.addItem('One')
+        self.DdlSnap.addItem('two')
+        self.DdlSnap.setMinimumWidth(175)
+        hboxSnapMenu.addWidget(self.DdlSnap)
+        self.vBox.addLayout(hboxSnapMenu)
+
+
 #------------------------------------------------------------------------------------------------------------------
 #Presets
 
 #buttons
         hboxPresets = QtWidgets.QHBoxLayout()
-        self.loadPreset = QtWidgets.QPushButton('Load Preset', self)
-        self.savePreset = QtWidgets.QPushButton('Save Preset', self)
-        hboxPresets.addWidget(self.loadPreset)
-        hboxPresets.addWidget(self.savePreset)
+        self.loadPresetBtn = QtWidgets.QPushButton('Load Preset', self)
+        self.savePresetBtn = QtWidgets.QPushButton('Save Preset', self)
+        hboxPresets.addWidget(self.loadPresetBtn)
+        hboxPresets.addWidget(self.savePresetBtn)
         self.vBox.addLayout(hboxPresets)
 
 
+#-------------------------------------------------------------------------------------------------------------------
+#BuildProject
+
+        hboxBuildProject = QtWidgets.QHBoxLayout()
+        self.buildProjectBtn = QtWidgets.QPushButton("Build", self)
+        self.buildProjectBtn.clicked.connect(self.buildProject)
+        hboxBuildProject.addWidget(self.buildProjectBtn)
+        self.vBox.addLayout(hboxBuildProject)
 
 
-
-
+#-------------------------------------------------------------------------------------------------------------------
+#Defines
         self.setLayout(self.vBox)
-        def buildProject(self):
-            print('hello world')
+
+    def textHasChangedBuildingDensity(self):
+        buildingDensity = self.textInputBuildingDensity.text()
+
+    def textHasChangedMaxFloors(self):
+        maxFloors = self.textInputMaxFloors.text()
+
+    def textHasChangedMinFloors(self):
+        minFloors = self.TextInputMinFloors.text()
+
+    def textHasChangedLocationX(self):
+        locationX = self.textInputLocationX.text()
+
+    def textHasChangedLocationY(self):
+        locationY = self.textInputLocationY.text()
+
+    def textHasChangedLocationZ(self):
+        locationZ = self.textInputLocationZ.text()
+    
+    def stateHasChangedSnapObj(self):
+        snapObj = self.DdlSnap
+
+    def stateHasChangedLoadPreset(self):
+        loadPreset = self.loadPresetBtn
+
+    def stateHasChangedSavePreset(self):
+        savePreset =  self.SavePresetBtn
+
+    def buildProject(self):
+#-------------------------------------------------------------------------------------------------------------------
+#Main Function
             
+        #Variables
+
+        buildingDensity = self.textInputBuildingDensity.text()
+        maxFloors = self.textInputMaxFloors.text()
+        minFloors = self.TextInputMinFloors.text()
+        locationX = self.textInputLocationX.text()
+        locationY = self.textInputLocationY.text()
+        locationZ = self.textInputLocationZ.text()
+        snapObj = self.DdlSnap
+        loadPreset = self.loadPresetBtn
+        savePreset =  self.savePresetBtn
+        timesran = 0
+        lowerFloorHeight = 2.7
+        heigherFloorHeight = 3.5
+            
+        myObj = hou.node('/obj')
+        geo = myObj.createNode("geo", 'city')
+        myGeo = hou.node('/obj/city')
+        subnet = myGeo.createNode("subnet", 'city')
+        mySub = hou.node('/obj/city/city')
+        merge = mySub.createNode("merge", 'merge1')
+
+
+
+        for i in range(int(buildingDensity)):
+            lowerTranslateX = random.randint(-100, 100)
+            lowerTranslateZ  = random.randint(-100, 100)
+
+
+
+            timesran += 1
+            box = mySub.createNode("box", f'building{timesran}')
+            transform = mySub.createNode("xform", f'tranform{timesran}')
+            transform.setInput(0, box)
+            randomFloors = random.randint((int(minFloors)), (int(maxFloors)))
+            randomFloorHeight = random.uniform(lowerFloorHeight, heigherFloorHeight)
+            boxHeight = randomFloorHeight * randomFloors
+
+
+            boxTranslateX = transform.parm('tx')
+            boxTranslateY = transform.parm('ty')
+            boxTranslateZ = transform.parm('tz')
+            boxHeightY = transform.parm('sy')
+            boxWidthX = transform.parm('sx')
+            boxWidthZ = transform.parm('sz')
+
+            boxTranslateX.set(int(locationX) + lowerTranslateX)
+            boxTranslateZ.set(int(locationY) + lowerTranslateZ)
+            boxTranslateY.set(int(locationY) + (int(boxHeight)/2))
+            boxHeightY.set(boxHeight)
+            boxWidthX.set(random.randint(8, 12))
+            boxWidthZ.set(random.randint(8, 12))
+
+            merge.setInput(timesran, transform)
+            box.setInput(0, mySub.indirectInputs()[0])
+            print("test")
+            print(randomFloors)
+
 dialog = cityBuilder()
 dialog.show()
+'''
+             )  
+          ( /(  
+   (      )\()) 
+   )\    ((_)\  
+  ((_)    _((_) 
+ _ | |   | || | 
+| || | _ | __ | 
+ \__/ (_)|_||_| 
+https://youtube.com/shorts/uWEIaF0PNGg?si=Yig-ik7Gy1xqwdkh
+'''            
